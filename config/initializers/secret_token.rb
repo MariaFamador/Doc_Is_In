3c74bd65f63f73e27d2688a59eac3046e44e25ae
Maria Famador <maria.famador87@gmail.com>
2013-12-10 23:33:48 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DocIsIn::Application.config.secret_key_base = '806b2f748bebf6136790bd59c3b96b40062726da0fdd45a62165b1a7ee3ec14b518a291349041fdd57b35af9f968af465acfdd3bcb5fe53fbcf4b2e518033f21'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DocIsIn::Application.config.session_store :cookie_store, key: '_DocIsIn_session'
