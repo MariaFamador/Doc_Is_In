128e2813fbcb9b8bbfdfd590417cb57bfe762d3e
Maria Famador <maria.famador87@gmail.com>
2014-01-30 01:39:13 +0800

    defined back_url method and applied in booking form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  helper_method :back_url
+
+  def after_sign_in_path_for(resource_or_scope)
+    dashboards_path
+  end 
+
+  def back_url
+    request.referer
+  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
   end
-
 end
-
