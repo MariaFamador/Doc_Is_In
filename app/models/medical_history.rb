4c2c97050ef6c6e7947d001bb0ff00e0c77c54e5
Maria Famador <maria.famador87@gmail.com>
2014-01-31 02:21:30 +0800

    updated medical history model

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,4 +1,12 @@
 class MedicalHistory < ActiveRecord::Base
-  ALLERGIES = %w[Penicillin Dust Seafoods]
-  belongs_to :patients
+  ALLERGIES = %w[Penicillin Dust Seafoods Fur Alcohol Antibiotics Peanuts Mushrooms].sort.push("Other").unshift("None")
+  PREVIOUS_DISEASES = %w[Measles Mumps Meningitis].sort.push("Other").unshift("None")
+
+  belongs_to :patient
+
+  validates :patient_id, presence: true
+
+  def update_constants
+
+  end
 end
