4bce01682ef01c25fedc00ad3dd9aab417fad47a
Maria Famador <maria.famador87@gmail.com>
2014-02-11 16:52:27 +0800

    modified appointments factory

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -2,11 +2,10 @@ FactoryGirl.define do
   factory :appointment do
     user
     patient
-    starts_at_time ("23 Jan 2014").to_time
     starts_at_date ("23 Jan 2014").to_date
+    starts_at_time ("9:30").to_time
+    starts_at ("23 Jan 2014 9:30").to_datetime
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    patient_id "43"
-    user_id "10"
   end
 end
