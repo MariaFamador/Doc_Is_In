8ba355368c0828d313429ca840b3af2b459913ea
Maria Famador <maria.famador87@gmail.com>
2014-02-13 00:42:05 +0800

    updated user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,20 +4,12 @@ describe User do
   let(:user) { create(:user) }
 
   context "Validations" do    
-    context 'when initial account details persisted' do
-      subject { create(:user) }
 
+    context "when new record" do 
       it { should validate_presence_of(:first_name) }
       it { should validate_presence_of(:last_name) }      
       it { should ensure_length_of(:first_name).is_at_most(50)}
       it { should ensure_length_of(:last_name).is_at_most(50)}
-    end
-
-    context "when new record" do 
-      it { should_not validate_presence_of(:first_name) }
-      it { should_not validate_presence_of(:last_name) }      
-      it { should_not ensure_length_of(:first_name).is_at_most(50)}
-      it { should_not ensure_length_of(:last_name).is_at_most(50)}
       it { should validate_presence_of(:email) }
       it { should validate_presence_of(:password) }
       it { should validate_presence_of(:password_confirmation) }
