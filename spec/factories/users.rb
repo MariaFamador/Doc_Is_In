90a09661c6c40cf8e4bed310c56d8fb161a840d2
Maria Famador <maria.famador87@gmail.com>
2014-02-13 00:43:32 +0800

    fixed erratic error of "Email Invalid" during tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do 
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { ("#{first_name}.#{last_name}@example.com").downcase }
+    email { Faker::Internet.email.downcase }
     password 'password'
     password_confirmation 'password'
     role 'doctor'
