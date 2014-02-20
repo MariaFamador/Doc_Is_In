04218a4e08869a4dd6febacdaea026f99ca833f3
Maria Famador <maria.famador87@gmail.com>
2014-02-20 16:56:29 +0800

    prescription model

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -1,8 +1,18 @@
 class Prescription < ActiveRecord::Base
+  attr_accessor :medicines_attributes, :symptoms_attributes
+
   has_many :medicines
   has_many :symptoms
+  
   belongs_to :patient
   belongs_to :user
 
+  accepts_nested_attributes_for :medicines, allow_destroy: true
+  accepts_nested_attributes_for :symptoms, allow_destroy: true
+
   validates :diagnosis, presence: true
+
+  def format_created_at
+    self.created_at.strftime("%A, %d %b")
+  end
 end
