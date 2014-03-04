19dbe967e85f5b392be7c504cdb68309466aa9c8
Maria Famador <maria.famador87@gmail.com>
2014-03-04 18:42:48 +0800

    spec for validation of medicine name in medicine_forms

--- spec/models/medicine_spec.rb
+++ spec/models/medicine_spec.rb
@@ -1,10 +1,22 @@
 require 'spec_helper'
 
 describe Medicine do
+  let(:medicine) { create(:medicine) }
   context "Associations" do
     it { should have_many(:symptoms).through(:prescription_guides) }
     it { should have_many(:prescription_guides) }
     it { should have_many(:dosages) }
     it { should belong_to(:prescription) }
+  end
+
+  context "Validations" do
+    context "when form is equal to medicine_form" do
+      subject do 
+        medicine.form = "medicine_form"
+        medicine
+      end
+
+      it { should validate_presence_of(:name) }
+    end
   end
 end
